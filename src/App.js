3c580e3c4f79db86ecdc0a6f8375cea66c82ccc7
Vinicius <vcatarino@outlook.com>
2017-09-09 23:51:27 -0300

    código rfatorado

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
 import Header from './componentes/Header';
 import Timeline from './componentes/TimeLine';
+import TimelineStore from './logicas/TimelineStore';
 
-class App extends Component {
+const timelineStore = new TimelineStore([]);
+
+class App extends Component { 
   render() {
     return (
       <div id="root">
       <div className="main">        
        <Header/>
-       <Timeline login={this.props.params.login}/>
+       <Timeline login={this.props.params.login} store={timelineStore}/>
       </div>
     </div>
     );
